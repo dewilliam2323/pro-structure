d96ebe686ae36a2e557b15c6baa26aa1e6ac302f
dewilliam2323 <williamogbebor@outlook.com>
2024-12-26 09:07:55 +0100

    this is pro structure code

---
+++ javascript/scrpts.js
@@ -0,0 +1,177 @@
+// console.log(" Hello Everyone");
+
+// let myAge = 20;
+// var myNumber = 30; 
+// const testNumber = 54;
+
+// console.log(testNumber);
+// let myName = "samuel";
+// console.log(myName);
+// let isAdmin = false;
+
+// let age;
+// console.log(age);
+// let data = null;
+
+// let mynewname = "sammy sam john";
+// console.log(mynewname);
+// mynewname ="john john sammy";
+// console.log(mynewname);
+
+// Arithemetic operator
+
+// let a = 78;
+// let b = 54;
+// let add = a = b;
+// let subtract = a - b;
+// let divide = a / b;
+// let mutiply = a * b;
+// let modulous = a % b;
+
+// console.log(add);
+// console.log(subtract);
+// console.log(divide);
+// console.log(mutiply);
+// console.log(modulous);
+
+// assignment  operator 
+
+
+// let x = 10
+// x +=5
+// console.log(x);
+
+// y = 20;
+// y -= 10;
+// console.log(y);
+
+// comparism operator
+
+// let a = 56
+// let b = 37
+// let c = 82
+// let d = 37;
+// let e = "37"
+
+// console.log(a == b);
+// console.log(b == d);
+// console.log(d == e);
+// console.log(a > b);
+// console.log(a < c);
+// console.log(a >= b);
+
+// // // // // // logical operator
+
+// // // // // const idcard = true;
+// // // // // const uniform = false;
+// // // // // const schfee = false;
+
+// // // // // console.log(idcard && uniform && schfee);
+// // // // // console.log(idcard || uniform || schfee);
+
+// // // // unary operators
+
+// let a = 20
+// a++;
+// console.log(a);
+
+// let b = 30;
+//  b--;
+//  console.log(b);
+
+//  tunary operators
+
+
+
+// Syntax
+// condition ? true : false 
+// let age = 13
+// let check = age >= 18 ? "this is an Adult" : "Not an adult";
+
+// console.log(check);
+
+// conditional statement 
+
+// If statement
+// Syntax
+
+// if(condition){
+
+//     what should happen when the condition is true
+// }
+
+// const username= "admin";
+// if (username === "admin"){
+
+//     console.log("welcome Admin");
+// }
+
+
+
+// If else statement
+// Syntax
+
+// if(condition){
+
+// what should happen when the condition is true
+
+// }else  {
+
+//      what should happen if the condition is not true
+// }
+
+// const userAge = 20;
+
+// if( userAge >18){
+//     console.log( "This user is liable to vote");
+
+// }else {
+
+//     console.log("This user is underage");
+// }
+
+    
+// / If else if statement
+// Syntax
+
+// if(condition){
+
+// what should happen when the condition is true
+
+// }else if(condition){
+
+//      what should happen if the first condition is not true
+// } else if (condition){
+//    what should happen if the second condition is not met
+
+// }else{
+
+//     what should happen if all the condition is not met
+// }
+
+// const userAge = 27;
+
+// if ( userAge <12){
+//     console.log("your are within the child age bracket");
+// }else if (userAge > 12 && userAge <19)(
+//     console.log("you are a teenager");
+// )else if (userAge > 19 && userAge <28){
+
+//     console.log("you are a youth");
+// }else {
+//     console.log("you are an adult");
+// }
+
+// Variable class work
+
+// let myname = "dewilliam";
+// console.log(myname);
+
+// Syntax class work pratice
+// condition ? true : false 
+
+// let age = 15
+
+// let check = age >= 25 ? "This is an adult" : "Not an adult";
+
+// console.log(check);
